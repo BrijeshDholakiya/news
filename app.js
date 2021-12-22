62697eb3db8a0d785ef06f4a4f7b3fd75723b2c0
Brijesh <95161078+BrijeshDholakiya@users.noreply.github.com>
2021-12-22 20:14:31 +0530

    Add edited file

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
 const api = {
-  key: "05071f9c6d7647d991cf76bb01d9c963",
-  base: "https://newsapi.org/v2/",
+  base: "https://newsdata.io/api/1/",
+  language:"en",
+  key: "pub_3012b04a8a8bf3d5aea9898ae21b3cbea977"
 }
+
 let newsAccordian = document.getElementById("newsAccordian");
 const searchFrom = document.querySelector(".searchFrom");
 const input = document.querySelector(".input");
 
 
 if (searchFrom.onsubmit == null) {
-  var urlLink = (`${api.base}top-headlines?country=in&apiKey=${api.key}`);
+  var urlLink = (`${api.base}news?country=in&language=${api.language}&apiKey=${api.key}`);
   fetchData(urlLink);
-} 
+}
 searchFrom.addEventListener("submit", ((e)=> {
   e.preventDefault();
   let topic = input.value;
-  var urlLink = (`${api.base}everything?q=${topic}&apiKey=${api.key}`);
+  var urlLink = (`${api.base}news?q=${topic}&language=${api.language}&apiKey=${api.key}`);
   fetchData(urlLink);
   searchFrom.reset();
 }));
 
 
-async function fetchData(url) {
+function fetchData(url) {
   var req = new Request(url);
- await fetch(req)
+  fetch(req)
     .then(response => response.json())
     .then((data) => {
       let newsData = "";
-      data.articles.forEach((article, index) => {
-        let items = ` <div class="accordion-item">
+      data.results.forEach((result, index) => {
+        let items = (` <div class="accordion-item">
                     <h2 class="accordion-header" id="heading${index}">
                       <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${index}" aria-expanded="true" aria-controls="collapse${index}">
-                      <strong>Breaking News ${index + 1}</strong> &nbsp ${article["title"]}
+                      <strong>Breaking News ${index + 1}</strong> &nbsp ${result["title"]}
                       </button>
                     </h2>
                     <div id="collapse${index}" class="accordion-collapse collapse" aria-labelledby="heading${index}" data-bs-parent="#accordionExample">
                       <div class="accordion-body">
-                        ${article["content"]} <a href="${article["url"]}" target="_blank">Read More here</a>
+                         ${result.description ? result.description.substr(0,150) + "...[+" + Math.abs(result.description.length-150)+"chars] " : "Sorry No Description available, For read about this news in detail click on the link --> "}<a href="${result["link"]}" target="_blank">Read More here</a>
                     </div>
+                    
                   </div>
                 </div>
-                    `
+                    `);
         newsData += items;
       });
       newsAccordian.innerHTML = newsData;
